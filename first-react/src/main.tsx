d788b19bc18b1af04fc653f1d1834eb5f02fa547
daniela-mendes <daniela.mendes@tecnico.ulisboa.pt>
2025-01-22 12:45:49 +0000

    ReactFacts Project

--- first-react/src/main.tsx
+++ first-react/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client' 
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = createRoot(document.getElementById("root")!)
+
+root.render(
+  <main>
+    <img src="src/assets/react.svg" width="40px" alt="react logo"/>
+    <h1>Fun facts about React!</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally cretated by Jordan Walke</li>
+      <li>Has well over 200k stars on GitHub</li>
+      <li>Is maintained by Meta</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </main>
 )
