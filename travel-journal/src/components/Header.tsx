9cd894b0de1f0faaa0116587f426e4d6ef54b5a0
daniela-mendes <daniela.mendes@tecnico.ulisboa.pt>
2025-01-23 21:30:21 +0000

    travel journal projecct: header

---
+++ travel-journal/src/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="/src/assets/globe.png" width="24px" alt="journal logo" />
+            <span>my travel journal.</span>
+        </header>
+    )
+}
