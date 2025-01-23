e61493788984a42fc96aec95061b9a2d3948a8a6
daniela-mendes <daniela.mendes@tecnico.ulisboa.pt>
2025-01-23 18:44:40 +0000

    ReactFacts project (static) - navbar and main content components; css manipulation; background image

---
+++ first-react/src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                <img src="/src/assets/react.svg" width="30px" alt="React logo" />
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
