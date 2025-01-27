a1ffc47e52731606f5b70a5fc05c8c1306c0cae2
daniela-mendes <daniela.mendes@tecnico.ulisboa.pt>
2025-01-27 13:19:21 +0000

    chef claude project: header and form

---
+++ chef-claude/src/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="src/assets/chef-claude-icon.png" width="40px" alt="chef claude logo" />
+            <span>Chef Claude</span>
+        </header>
+    )
+}
---
+++ chef-claude/src/Main.tsx
@@ -0,0 +1,10 @@
+export default function Main() {
+    return (
+        <main>
+            <form>
+                <input type="text" placeholder="e.g. oregano" aria-label="add  ingredient" />
+                <button>+ Add ingredient</button>
+            </form>
+        </main>
+    )
+}
