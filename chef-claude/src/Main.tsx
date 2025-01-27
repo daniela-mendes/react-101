6dfa976ce2811f38942eccc2e91dfc653b23fd45
daniela-mendes <daniela.mendes@tecnico.ulisboa.pt>
2025-01-27 14:12:17 +0000

    chef claude: - iterating over ingredients list and rendering them - event listener for submit - form adds ingredient to the list, but does not render the new element

--- chef-claude/src/Main.tsx
+++ chef-claude/src/Main.tsx
@@ -1,10 +1,27 @@
 export default function Main() {
+    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    
+    const ingredientsList = ingredients.map(ing => (
+        <li key={ing}>{ing}</li>
+    ))
+
+    function handleSubmit(event: any) {
+        event.preventDefault() // to avoid page refresh
+        const formData: FormData = new FormData(event.currentTarget)
+        const newIngredient: any = formData.get("ingredient")
+        ingredients.push(newIngredient)
+        console.log(ingredients)
+    }
+
     return (
         <main>
-            <form>
-                <input type="text" placeholder="e.g. oregano" aria-label="add  ingredient" />
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="e.g. oregano" aria-label="add ingredient" name="ingredient" />
                 <button>+ Add ingredient</button>
             </form>
+            <ul>
+                {ingredientsList}
+            </ul>
         </main>
     )
 }
