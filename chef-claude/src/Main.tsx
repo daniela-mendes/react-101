8762a4aba04059b11d9bd03ee3c9863e0c2fe7ca
daniela-mendes <daniela.mendes@tecnico.ulisboa.pt>
2025-01-29 13:40:55 +0000

    chef claude: conditional rendering - only displaying ingredients list when user adds ingredients and recipe generator when user has provided 4+ ingredients

--- chef-claude/src/Main.tsx
+++ chef-claude/src/Main.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react"
 
 export default function Main() {
-    const [ingredients, setIngredients] = useState(["Chicken", "Oregano", "Tomatoes"])
+    const [ingredients, setIngredients] = useState<string[]>([])
     
     const ingredientsList = ingredients.map(ing => (
-        <li key={ing}>{ing}</li>
+        <li className="ingredients-list-items" key={ing}>{ing}</li>
     ))
 
     function handleSubmit(formData: FormData) {
@@ -20,9 +20,19 @@ export default function Main() {
                 <input type="text" placeholder="e.g. oregano" aria-label="add ingredient" name="ingredient" />
                 <button>+ Add ingredient</button>
             </form>
-            <ul>
-                {ingredientsList}
-            </ul>
+            {ingredients.length > 0 && <section>
+                <h2>Ingredients on hand:</h2>
+                <ul>
+                    {ingredientsList}
+                </ul>
+                {ingredients.length > 3 ? <div className="get-recipe">
+                    <div className="get-recipe-text">
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div> : null}
+            </section>}
         </main>
     )
 }
