0a2d99f90f211d2b1b9dd2485ad1b5544bc4d3df
daniela-mendes <daniela.mendes@tecnico.ulisboa.pt>
2025-02-03 21:50:04 +0000

    chef claude: suggested recipe styling and formatting

--- chef-claude/src/ClaudeRecipe.tsx
+++ chef-claude/src/ClaudeRecipe.tsx
@@ -1,7 +1,10 @@
+import ReactMarkdown from "react-markdown";
+
 export default function ClaudeRecipe(props: {recipe: string}) {
     return (
         <section>
-            {props.recipe}    
+            <h2>Suggested recipe:</h2>
+            <ReactMarkdown className="suggested-recipe">{props.recipe}</ReactMarkdown>
         </section>
     )
 }
