54191de5b594a920f1d7b50a7d083ef6952e8e6c
daniela-mendes <daniela.mendes@tecnico.ulisboa.pt>
2025-02-07 22:06:19 +0000

    tenzies capstone: dice rendered automatically by mapping over the array of random numbers saved in react state

--- tenzies/src/App.tsx
+++ tenzies/src/App.tsx
@@ -1,21 +1,26 @@
+import { useState } from "react"
 import Dice from "./Dice"
 
 export default function App() {
+	const [diceNumbers, setDiceNumbers] = useState(generateAllNewDice())
+
+	function generateAllNewDice() {
+        const diceArray = []
+        for (let i = 0; i < 10; i++)
+            diceArray.push(Math.floor(Math.random() * 6) + 1);
+        return diceArray
+    }
+
+	const dice = diceNumbers.map((num) => (
+		<Dice value={num} />
+	))
+
   	return (
 		<main>
 			<span className="title">Tenzies</span>
 			<span className="description">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</span>
 			<div className="grid">
-				<Dice value={Math.floor(Math.random() * 6) + 1} />
-				<Dice value={Math.floor(Math.random() * 6) + 1} />
-				<Dice value={Math.floor(Math.random() * 6) + 1} />
-				<Dice value={Math.floor(Math.random() * 6) + 1} />
-				<Dice value={Math.floor(Math.random() * 6) + 1} />
-				<Dice value={Math.floor(Math.random() * 6) + 1} />
-				<Dice value={Math.floor(Math.random() * 6) + 1} />
-				<Dice value={Math.floor(Math.random() * 6) + 1} />
-				<Dice value={Math.floor(Math.random() * 6) + 1} />
-				<Dice value={Math.floor(Math.random() * 6) + 1} />
+				{dice}
 			</div>
 			<button>Roll</button>
 		</main>
